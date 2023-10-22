53a824335b6d8abe9ed992a23a224605159c6bd9
MD <25043596+MD-2016@users.noreply.github.com>
2023-10-22 14:46:49 -0400

    implemented a rate limiter using mutex's and ip checks

--- limit.go
+++ limit.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
@@ -45,11 +48,61 @@ func rateLimiter(next func(w http.ResponseWriter, r *http.Request)) http.Handler
 }
 
 func perClientRateLimiter(next func(writer http.ResponseWriter, request *http.Request)) http.Handler {
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
 
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			// Lock the mutex to protect from race conditions
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//extract the ip from request
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// lock the mutex to protect from race condition
+		mu.Lock()
+		if _, found := clients[ip]; !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+		}
+		clients[ip].lastSeen = time.Now()
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock()
+
+			message := Message{
+				Status: "Request Failed",
+				Body:   "The API capacity is met",
+			}
+			w.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(w).Encode(&message)
+			return
+		}
+		mu.Unlock()
+		next(w, r)
+	})
 }
 
 func main() {
-	http.Handle("/ping", rateLimiter(endpointHandler))
+	//http.Handle("/ping", rateLimiter(endpointHandler))
+	http.Handle("/ping", perClientRateLimiter(endpointHandler))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Println("error listening on port 8080", err)
